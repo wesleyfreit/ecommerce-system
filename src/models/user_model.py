31f77f6d9360ffa2140563dc1033c0cb092e08e4
Richard Freitas <wesleyrichard30@gmail.com>
2024-01-19 13:50:57 -0300

    feat: create user model

---
+++ src/models/user_model.py
@@ -0,0 +1,11 @@
+from db.instance import db
+from flask_login import UserMixin
+from sqlalchemy import UUID
+import uuid
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+    is_admin = db.Column(db.Boolean, nullable=False, default=False)
