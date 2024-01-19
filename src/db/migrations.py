73e705ea6e042ef8f9621e331cfbcd20e2a5e58b
Richard Freitas <wesleyrichard30@gmail.com>
2024-01-19 07:36:49 -0300

    feat: change integer id as uuid type

--- src/db/migrations.py
+++ src/db/migrations.py
@@ -1,10 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import UUID
+import uuid
+
 
 db = SQLAlchemy()
 
 
 class Product(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = db.Column(db.String(120), nullable=False)
     price = db.Column(db.Float, nullable=False)
     description = db.Column(db.Text, nullable=True)
